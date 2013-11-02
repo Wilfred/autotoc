2e967db07a1b78893747b551de7cb4f550cc4e26
Wilfred Hughes <me@wilfred.me.uk>
2013-11-02 19:18:08 +0000

    initial commit with skeleton project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to autotoc
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject autotoc "0.1.0-SNAPSHOT"
+  :description "A markdown TOC generator"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
