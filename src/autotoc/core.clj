90451335bae40b4ab03ce31b18e0bd28e3952653
Wilfred Hughes <me@wilfred.me.uk>
2013-11-03 18:07:42 +0000

    build a TOC tree from weighted headings

--- src/autotoc/core.clj
+++ src/autotoc/core.clj
@@ -1,5 +1,5 @@
 (ns autotoc.core
-  (:require [clojure.string :refer [split-lines trim]]))
+  (:require [clojure.string :refer [split-lines trim join]]))
 
 (defn get-headings
   "Return a vector of headings in the given markdown source."
@@ -16,6 +16,21 @@
   [heading]
   (let [[_ prefix text] (re-find #"^(#+)(.*?)#*$" (trim heading))]
     (list (count prefix) (trim text))))
+
+(defn build-toc-tree
+  "Return a markdown nested list of bullets for this table of contents."
+  [weighted-headings]
+  (join "\n"
+        (map (fn [[weight text]]
+               (format "%s- [%s](#%s)"
+                       (repeat-string "  " (dec weight))
+                       text text))
+             weighted-headings)))
+
+(defn repeat-string
+  "Return a string that is the original string repeated n times."
+  [string n]
+  (apply str (repeat n string)))
 
 (defn -main
   "I don't do a whole lot."
