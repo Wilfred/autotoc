4896a77e5d1f497bea9328f7c6cc68512c8b483f
Wilfred Hughes <me@wilfred.me.uk>
2013-11-03 17:45:45 +0000

    extract headings from markdown source

--- src/autotoc/core.clj
+++ src/autotoc/core.clj
@@ -1,6 +1,17 @@
-(ns autotoc.core)
+(ns autotoc.core
+  (:require [clojure.string :refer [split-lines]]))
 
-(defn foo
+(defn get-headings
+  "Return a vector of headings in the given markdown source."
+  [markdown]
+  (loop [headings [] lines (split-lines markdown) in-code false]
+    (let [line (first lines)]
+      (cond
+       (empty? lines) headings
+       (.startsWith (trim line) "#") (recur (conj (trim headings) line) (rest lines) in-code)
+       :else (recur headings (rest lines) in-code)))))
+
+(defn -main
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
