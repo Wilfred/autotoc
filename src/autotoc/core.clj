e6d0acd5c115e46cd3b021421bc37cddd7e01073
Wilfred Hughes <me@wilfred.me.uk>
2013-11-03 17:56:34 +0000

    extract heading weights from markdown

--- src/autotoc/core.clj
+++ src/autotoc/core.clj
@@ -1,5 +1,5 @@
 (ns autotoc.core
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [clojure.string :refer [split-lines trim]]))
 
 (defn get-headings
   "Return a vector of headings in the given markdown source."
@@ -10,6 +10,12 @@
        (empty? lines) headings
        (.startsWith (trim line) "#") (recur (conj (trim headings) line) (rest lines) in-code)
        :else (recur headings (rest lines) in-code)))))
+
+(defn add-weight
+  "Strip # characters from a heading and return a list (weight, text-content)."
+  [heading]
+  (let [[_ prefix text] (re-find #"^(#+)(.*?)#*$" (trim heading))]
+    (list (count prefix) (trim text))))
 
 (defn -main
   "I don't do a whole lot."
