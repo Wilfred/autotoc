4739f506204c5442b8878a122d0fd9088cdd662c
Wilfred Hughes <me@wilfred.me.uk>
2013-11-28 15:48:53 +0000

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject autotoc "0.2.0-SNAPSHOT"
+(defproject autotoc "0.3.0-SNAPSHOT"
   :description "A markdown TOC generator"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
